00ae512b62c560ad5edb820de88199bd0254b7c6
ravagefox <44389726+ravagefox@users.noreply.github.com>
2023-03-02 16:25:01 +1100

    Add files via upload

---
+++ 00 - CORE/Fallen.Client/Extensions/PacketReaderExtensions.cs
@@ -0,0 +1,33 @@
+﻿// Source: PacketReaderExtensions
+/* 
+   ---------------------------------------------------------------
+                        CREXIUM PTY LTD
+   ---------------------------------------------------------------
+
+     The software is provided 'AS IS', without warranty of any kind,
+   express or implied, including but not limited to the warrenties
+   of merchantability, fitness for a particular purpose and
+   noninfringement. In no event shall the authors or copyright
+   holders be liable for any claim, damages, or other liability,
+   whether in an action of contract, tort, or otherwise, arising
+   from, out of or in connection with the software or the use of
+   other dealings in the software.
+*/
+
+using Engine.Net.Messaging;
+using Microsoft.Xna.Framework;
+
+namespace Client.Extensions
+{
+    internal static class PacketReaderExtensions
+    {
+        public static Vector4 ReadVector4(this PacketReader reader)
+        {
+            return new Vector4(
+                reader.ReadSingle(),
+                reader.ReadSingle(),
+                reader.ReadSingle(),
+                reader.ReadSingle());
+        }
+    }
+}
